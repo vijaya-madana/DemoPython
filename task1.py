633c9e6dd6638a3325ac4c705acfca65e0ed7f61
vijaya-madana <vijayamadana409@gmail.com>
2023-06-12 15:37:38 +0530

    added a file task1.py

---
+++ task1.py
@@ -0,0 +1,3 @@
+#1.1 Write a program to display any message
+
+print(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of experience in Coartha techno solutions pvt ltd as a software engineer")
