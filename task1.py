45c0d544b6ca1812dbef2b276613a0c8ae60b3f8
vijaya-madana <vijayamadana409@gmail.com>
2023-06-12 15:46:03 +0530

    changes in a file task1.py

--- task1.py
+++ task1.py
@@ -3,7 +3,8 @@ class Task1:
     def task1(self):
         print(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of experience in Coartha techno solutions pvt ltd as a software engineer")
 
-        
+
 if __name__ == "__main__":
     obj = Task1()
     obj.task1()
+    
