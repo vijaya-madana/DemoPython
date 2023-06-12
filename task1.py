0ab42bd20add7529cc2da25a44fa766b56efd725
vijaya-madana <vijayamadana409@gmail.com>
2023-06-12 15:45:18 +0530

    changes in a file task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,9 @@
 #1.1 Write a program to display any message
+class Task1:
+    def task1(self):
+        print(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of experience in Coartha techno solutions pvt ltd as a software engineer")
 
-print(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of experience in Coartha techno solutions pvt ltd as a software engineer")
+        
+if __name__ == "__main__":
+    obj = Task1()
+    obj.task1()
